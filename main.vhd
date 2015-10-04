c6d5fc0c8e48c54dd203c44203eeeed4922e3116
Robin Janssens <robin@robinjanssens.net>
2015-10-04 22:01:52 +0200

    added i2c master library

--- main.vhd
+++ main.vhd
@@ -32,14 +32,30 @@ ARCHITECTURE Behavioral of main is
   -- =============================
   -- Components
   -- =============================
-
+  component i2c_master
+    GENERIC(
+      input_clk : INTEGER := 50_000_000; --input clock speed from user logic in Hz
+      bus_clk   : INTEGER := 400_000);   --speed the i2c bus (scl) will run at in Hz
+    PORT(
+      clk       : IN     STD_LOGIC;                     --system clock
+      reset_n   : IN     STD_LOGIC;                     --active low reset
+      ena       : IN     STD_LOGIC;                     --latch in command
+      addr      : IN     STD_LOGIC_VECTOR (6 DOWNTO 0); --address of target slave
+      rw        : IN     STD_LOGIC;                     --'0' is write, '1' is read
+      data_wr   : IN     STD_LOGIC_VECTOR (7 DOWNTO 0); --data to write to slave
+      busy      : OUT    STD_LOGIC;                     --indicates transaction in progress
+      data_rd   : OUT    STD_LOGIC_VECTOR (7 DOWNTO 0); --data read from slave
+      ack_error : BUFFER STD_LOGIC;                     --flag if improper acknowledge from slave
+      sda       : INOUT  STD_LOGIC;                     --serial data output of i2c bus
+      scl       : INOUT  STD_LOGIC);                    --serial clock output of i2c bus
+  end component;
 
 BEGIN
 
   -- =============================
   -- Port Mappings
   -- =============================
-
+  
 
   -- =============================
   -- Main Code
